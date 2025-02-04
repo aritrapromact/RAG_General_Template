f6e65f71094031b9d6169db0e918312a8e9a8ca4
lakshaypromact <lakshayaggarwal@promactinfo.com>
2025-02-04 10:54:50 +0530

    added sqlite along with postgresql in settings file

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,17 +2,17 @@
 This file contains configuration settings for the application,
 such as environment variables and constants.
 """
-
 import os
-from pathlib import Path 
+from pathlib import Path
 from dotenv import main
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 main.load_dotenv()
 
-
-
 def load_env_var_strict(name: str) -> str:
-    """This function loads an environment variable with the specified name. If the environment variable is not set, it raises
-    a ValueError."""
+    """Loads an environment variable and raises an error if not set."""
     value = os.getenv(name)
     if value is None:
         raise ValueError(f"Environment variable {name} is not set")
@@ -22,46 +22,36 @@ JWT_AUTH_SECRET_KEY = load_env_var_strict('JWT_AUTH_SECRET_KEY')
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 50
 
-
+## Configuration Settings
+DB_TYPE = os.getenv("DB_TYPE", "sqlite").lower()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# # PostgreSQL Database Configuration
-POSTGRES_USER = load_env_var_strict('POSTGRES_USER')
-POSTGRES_DATABASE = load_env_var_strict('POSTGRES_DATABASE')
-POSTGRES_PASSWORD = load_env_var_strict('POSTGRES_PASSWORD')
-POSTGRES_HOST = load_env_var_strict('POSTGRES_HOST')
-POSTGRES_PORT = load_env_var_strict('POSTGRES_PORT')
+if DB_TYPE == "postgres":
+    # PostgreSQL Database Configuration
+    POSTGRES_USER = load_env_var_strict('POSTGRES_USER')
+    POSTGRES_DATABASE = load_env_var_strict('POSTGRES_DATABASE')
+    POSTGRES_PASSWORD = load_env_var_strict('POSTGRES_PASSWORD')
+    POSTGRES_HOST = load_env_var_strict('POSTGRES_HOST')
+    POSTGRES_PORT = load_env_var_strict('POSTGRES_PORT')
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
+else:
+    # SQLite Database Configuration
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{BASE_DIR}/db.sqlite3"
 
-
-## ORM Engine Configuration
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
+# ORM Engine Configuration
 
 GROQ_API_KEY = load_env_var_strict('GROQ_INFERENCE_API_KEY')
 GROQ_MODEL_NAME  = "mixtral-8x7b-32768"
 
-
-
-### Database Configuration
-
-from app.config.settings import SQLALCHEMY_DATABASE_URL
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# # SQLite setup
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-print(SQLALCHEMY_DATABASE_URL)
-# # Postgres setup
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if DB_TYPE == "sqlite" else {})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# BASE ORM Model 
+# Base ORM Model
 Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
-Base.metadata.create_all(bind=engine) 
-
-# DB Utilities 
+# DB Utilities
 def get_session():
     db = SessionLocal()
     try:
